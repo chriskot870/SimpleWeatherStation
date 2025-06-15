629b88a10a4d36183540c727a7810f4307fc3371
Chris Kottaridis <chris@quietwind.net>
2025-06-14 19:35:53 -0500

    Change debug IP address of target and copy config
    We are going to use a new host for debug.
    
    Also, when debugger starts up copy the config file to target as well as
    the executable.

--- cmake/start_gdbserver.sh
+++ cmake/start_gdbserver.sh
@@ -18,6 +18,17 @@ then
 fi
 
 #
+# Make sure the config file is available
+#
+build_config_cksum=`cksum $build_config 2>/dev/null | awk '{print($1)}'`
+    
+if [ -z  $build_config_cksum ]
+then
+    echo No build config file
+    exit 1
+fi
+
+#
 # We need to copy the executable over to the target so that
 # gdbserver can access it. We check if the files exists on the
 # target. If it does then we get the checksum. Otherwise, we
@@ -45,6 +56,36 @@ then
     scp $build_command $target_user@$target_ip:$target_command 1>/dev/null 2>&1
 else
     echo Using existing build command on target
+fi
+
+#
+# We need to copy the config over to the target so that
+# the executable can access it. We check if the file exists on the
+# target. If it does then we get the checksum. Otherwise, we
+# set the checksum to zero so it won't match the newly built
+# executable. That will force a copy.
+#
+ssh $target_user@$target_ip test -f $target_config
+if [ $? -eq 0 ]
+then
+   echo Getting target cksum
+   target_config_cksum=`ssh $target_user@$target_ip "cksum $target_config 2>/dev/null" | awk '{print($1)}'`
+else
+  target_config_cksum="0"
+fi
+
+#
+# If the cksums are different then we need to copy the config to the target
+#
+if [ $build_config_cksum != $target_config_cksum ]
+then
+    #
+    # If the cksums don't match copy the new build config to the target
+    #
+    echo Copying new configuration to target
+    scp $build_config $target_user@$target_ip:$target_config 1>/dev/null 2>&1
+else
+    echo Using existing configuration on target
 fi
 
 #
